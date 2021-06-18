ff1f32e8d60244835b6862966f63c72b439859b2
tustustushar <71062376+tustustushar@users.noreply.github.com>
2021-06-18 09:37:58 +0530

    Update __init__.py

--- lycia/__init__.py
+++ lycia/__init__.py
@@ -1,8 +1,8 @@
 from lycia.config import Config
 from pyrogram import Client
 
-API_ID = Config.API_ID
+APP_ID = Config.APP_ID
 API_HASH = Config.API_HASH
 TOKEN = Config.TOKEN
 
-LYCIA = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+LYCIA = Client(':memory:', app_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
